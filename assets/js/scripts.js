282bcb7e81c066429eb14b0810c3ca34ab51628e
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-11-10 17:40:57 +0000

    margins

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,9 @@
 $('.hamburger').click(function() {
     $('.hamburger').toggleClass("is-active");
     $('.navbar').toggleClass('navbar-open');
-  });
+});
+
+$('.navbar a').click(function() {
+    $('.hamburger').toggleClass("is-active");
+    $('.navbar').toggleClass('navbar-open');
+});
